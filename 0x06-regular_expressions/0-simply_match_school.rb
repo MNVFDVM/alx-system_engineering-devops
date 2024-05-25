0d120ffd8ef6bb0664e74d18beca357d97b9480a
medo <mohammedabdelhamed725@gmail.com>
2024-05-25 04:07:08 -0700

    sdf

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-	puts "Usage: #{$0} <string>"
-	exit 1
-end
-
-regex = /School/
-input = ARGV[0]
-
-# Ensure input is a string before scanning
-if input.is_a?(String)
-	matches = input.scan(regex)
-	puts matches.join
-else
-	puts "Error: Input is not a valid string."
-end
+puts ARGV[0].scan(/School/)
