17eb2276269591b24da781f9cc67987b65cb9fc0
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-07-04 15:59:49 -0700

    df

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-#This code will install the package puppet-lint
-package { 'flask':
-  ensure    =>  '2.1.0',
-  provider  =>  'pips',
+# This manifest installs Flask version 2.1.0 using pip3
+
+# Ensure that python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Ensure that Flask version 2.1.0 is installed using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
