d12b138b68ded5031ced374f37df1b58bbde3f6d
MNVFDVM <mohammedabdelhamed725@gmail.com>
2024-09-26 21:46:20 -0700

    sdf

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle by modifying the default file.
-
-# Increase the ULIMIT of the default file
-exec { 'fix-ulimit-for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin'],
-  onlyif  => 'grep -q "15" /etc/default/nginx',  # Ensure the change is applied only if needed
-}
-
-# Restart Nginx after fixing ULIMIT
-exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',   # Using init.d to restart Nginx
-  path    => ['/sbin', '/bin', '/usr/sbin', '/usr/bin'],
-  subscribe => Exec['fix-ulimit-for-nginx'],  # Restart nginx only if the ULIMIT change happens
-  refreshonly => true,  # Only restart when there's a change
+# Manifest to fix Nginx open file limit configuration
+exec { 'nginx fix':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  provider => 'shell'
 }
